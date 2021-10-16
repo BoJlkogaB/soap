6e238a96050f53748e44fd5dab1bd856ea2e33c6
BoJlkogaB <56555887+BoJlkogaB@users.noreply.github.com>
2021-10-16 18:17:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    require ($_SERVER['DOCUMENT_ROOT'].'/recloud-test-task/templates/layouts/head.php');
-    require ($_SERVER['DOCUMENT_ROOT'].'/recloud-test-task/templates/layouts/header.php');
-    require ($_SERVER['DOCUMENT_ROOT'].'/recloud-test-task/templates/layouts/main.php');
-    require ($_SERVER['DOCUMENT_ROOT'].'/recloud-test-task/templates/layouts/footer.php');
+    require ($_SERVER['DOCUMENT_ROOT'].'/templates/layouts/head.php');
+    require ($_SERVER['DOCUMENT_ROOT'].'/templates/layouts/header.php');
+    require ($_SERVER['DOCUMENT_ROOT'].'/templates/layouts/main.php');
+    require ($_SERVER['DOCUMENT_ROOT'].'/templates/layouts/footer.php');
 ?>
